8c525f533ed2737548315b733e2dcf64d6f46ebf
AviFelman <avi.felmans@gmail.com>
2019-04-09 00:19:20 -0700

    "Various fixes & name changes"

--- nomicsREST.py
+++ nomicsREST.py
@@ -12,7 +12,9 @@ from pandas.io.json import json_normalize
 
 # Create the nomicsREST API class, which will contain specific functions
 class nomicsREST(object):
-    # Initialize the class with API key and a URL (if Nomics changes it's url, or you're using a testnet otherwise leave blank)
+
+    # Initialize the class with API key and a URL
+    # The URL is included just in the case that Nomics changes it's url, or you're using a testnet
     def __init__ (self, key=None, url=None):
         self.key = key
         if url:
@@ -20,75 +22,89 @@ class nomicsREST(object):
         else:
             self.url = 'https://api.nomics.com/v1/'
 
-    # This is a function for pulling the JSON from the API call. It will be used in all further functions
+    # This is a function for pulling the JSON data from Nomics. It will be used in all further functions
     def request(self, action):
         response = requests.get(self.url + action)
-        # If you haven't provided an API key, this will notify you
+
+        # Catching errors
         if self.key is None:
             raise Exception('API key is empty')
-
         if response.status_code != 200:
             raise Exception("Error: " + str(response.status_code) + "\n" + response.text + "\nRequest: " + self.url + action)
 
         json = response.json()
         return json
 
-    # Get JSON list of all current currencies
+    # Get list of all currencies tracked by nomics (Return: JSON)
     def get_currencies(self):
         return self.request('currencies?key={}'.format(self.key))
 
-    # List of all currencies with current prices
+    # List of all currencies with current prices (Return: JSON)
     def get_current_prices(self):
         return self.request('prices?key={}'.format(self.key))
 
-    # General overview of all currencies & their current statistics
-    def get_overview(self):
+    # Dashboard view of all currencies (Return: JSON)
+    def get_dashboard(self):
         return self.request('dashboard?key={}'.format(self.key))
 
-    # Historic overall marketcap for all cryptocurrencies
-    # Begins on Start Date at 4:00pm PT / 0:00 UTC. Format is: YYYY-MM-DD
+    # Historic total market capitalization (Return: JSON)
+    # Begins on Start Date at 4:00pm PT / 0:00 UTC. Format for date is: YYYY-MM-DD
     def get_overall_marketcap(self, start_date, end_date):
         return self.request('market-cap/history?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, end_date))
 
-    # Gets 1d candle history for specific coins over time
+    # Daily candles for one currency at a time (Return: JSON)
+    # Begins on Start Date at 4:00pm PT / 0:00 UTC. Format for date is: YYYY-MM-DD
     def get_price_history(self, start_date, end_date, coin):
         return self.request('candles?key={}&interval=1d&currency={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, coin, start_date, end_date))
 
-    # Pulls a historic snapshot of all currencies and displays their pricing data. This works for
+    # Pulls all currencies and their prices over a set period of time (Return: JSON)
     def get_sparkline_data(self, start_date, end_date=None):
-        if end_date is not None:
-            return self.request('currencies/sparkline?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, end_date))
-        else:
-            return self.request('currencies/sparkline?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, start_date))
+        if end_date is None:
+            end_date = start_date
+        return self.request('currencies/sparkline?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, end_date))
 
-    # Only works after January 2018
+    # Note: Only works after January 2018 (Return: JSON)
+    # Pulls all currencies and their supply data over a set period of time
     def get_supply_data(self, start_date, end_date=None):
-        if end_date is not None:
-            return self.request('supplies/interval?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, end_date))
-        else:
-            return self.request('supplies/interval?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, start_date))
+        if end_date is None:
+            end_date = start_date
+        return self.request('supplies/interval?key={}&start={}T00%3A00%3A00Z&end={}T00%3A00%3A00Z'.format(self.key, start_date, end_date))
 
-    # Get the marketcap ranking of assets on any given day in history
-    # This returns the marketcap at the beginning of the start date mentioned UTC time. For example, inputting '2019-01-01' will pull the marketcap of a specific coin @ 0:00 UTC on January 1st, 2019
-
-    def get_marketcap_data(self, start_date, coin=None):
+    # Pull the ranking of cryptocurrencies by marketcap at any given day in history (Return: DataFrame)
+    # Inputting '2019-01-01' will pull the marketcap of a specific coin @ 0:00 UTC on January 1st, 2019
+    def get_marketcap_snapshot(self, start_date, coin=None):
+        # Pull both the supply and price data at a specific point in time
         prices = json_normalize(self.get_sparkline_data(start_date))
         supply = json_normalize(self.get_supply_data(start_date))
 
-        # This line extracts prices from the original dataframe to allow for easy manipulation
-        prices['prices'] = [item['prices'][0] for item in self.get_sparkline_data(start_date)]
+        # This line extracts prices from the original dataframe
+        prices['prices'] = [item['prices'][0] for item in prices]
 
         # Merge the dataframes to make multiplication easy
         market_cap_data = prices.merge(supply, how='left', on='currency')[['currency', 'prices', 'close_available']]
 
-        ## The only way to get market cap from Nomics data is to multiply supply and price together
+        # The only way to get market cap from Nomics data is to multiply supply and price together
         market_cap_data['market_cap'] = pd.to_numeric(market_cap_data['prices']) * pd.to_numeric(market_cap_data['close_available'])
 
-        ## This piece of code is to pull out a specific coin's data if you only wanted 1
+        # This piece of code is to pull out a specific coin's data if you only wanted 1
         if(coin is not None):
             return market_cap_data[market_cap_data['currency'] == coin]
         else:
-            return market_cap_data
+            # Sort the data for ease of use
+            return market_cap_data.sort_values(['market_cap'], ascending=False, inplace=True)
+
+    def get_historic_marketcap(self, coin, start_date, end_date):
+        df_final = pd.DataFrame()
+        date_list = pd.date_range(start_date, end_date).tolist()
+        market_cap = []
+        for date1 in date_list:
+            date2 = date1.date()
+            df_temp = self.get_marketcap_data(date2, 'BTC')
+            df_temp.reset_index(inplace=True)
+            market_cap.append(df_temp['market_cap'][0])
+        df_final['date'] = date_list
+        df_final['marketcap'] = market_cap
+        return df_final
 
 
     # price_to_dataframe allows you to input a list of coins and get their historic prices at the open of the days you've specificied. The specific column input is there to allow you to remove the returns column or the prices column if need be.
@@ -96,14 +112,24 @@ class nomicsREST(object):
     def get_multiple_coin_prices(self, coin_list, start_date, end_date, specific_column=None):
         df_final = pd.DataFrame()
         for coin in coin_list:
+
             coin_open = str(coin) + 'open'
             coin_returns = str(coin) + 'returns'
-            df_temp = pd.DataFrame().from_dict(self.get_price_history(start_date, end_date, coin))[['close', 'timestamp']]
+
+            df_temp = pd.DataFrame().from_dict(self.get_price_history(start_date, end_date, coin))
+
+            if(df_temp.empty):
+                raise Exception(coin + " is not in nomics dataset")
+            else:
+                df_temp = df_temp[['open', 'timestamp']]
+
             df_temp.rename(columns={'open': coin_open}, inplace=True)
             df_temp[coin_returns] = pd.to_numeric(df_temp[coin_open]).pct_change(1)
 
+            ## Following statements allow you to pull specific columbs
             if(specific_column =='returns'):
                 df_temp.drop([coin_close], axis=1, inplace=True)
+
             if(specific_column == 'prices'):
                 df_temp.drop([coin_returns], axis=1, inplace=True)
 
@@ -121,18 +147,5 @@ if __name__ == '__main__':
     '''
 
     nr = nomicsREST('')
-
-    #coin_list = ["BTC","ETH","XRP","LTC","EOS","BCH","BNB","XLM","USDT","TRX","ADA",
-    #             "BSV","XMR","DASH","MKR","ONT","NEO","ETC","XTZ","XEM","ZEC",
-    #             "VET","WAVES","USDC","DOGE","BAT","BTG","QTUM","OMG","TUSD",
-    #             "DCR","LSK","REP","LINK","ZIL","HOT","ZRX","RVN","ICX","DGB",
-    #             "ENJ","STEEM","BCN","BTS","BTT","NANO","BCD","HT","AE","PAX",
-    #             "BTM","KMD","XVG","NPXS","IOST","SC","THETA",
-    #             "MXM","DAI","STRAT","SNT","GNT","PAI","PPT","ARDR",
-    #             "ARK","GXC","R","CNX","GUSD","FCT","MAID","WAX","HC","LOOM",
-    #             "ETN","DGTX","QASH","WTC","MANA","MCO","LRC","ELF","THR","XZC",
-    #             "PIVX","NEXO","KNC","AION","POWR","WAN","ZEN","ETP","ELA","NAS",
-    #             "ODE","BNT","DENT","STORJ","NULS","KIN","GRS","DGD","RDD","POLY",
-    #             "BIX","MONA","QNT","QKC","SYS","ENG"]
-
-    #prices = nr.price_to_dataframe(['XVG'], '2019-03-29', '2019-03-29', 'prices')
+    coin_list = ['BTC', 'ETH', 'NANO'## to the moon]
+    prices = nr.get_historic_marketcap('BTC','2019-03-20', '2019-04-09')
